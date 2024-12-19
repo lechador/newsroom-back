777074c6bbddc31cf50154fda2b758424237b716
lechador <135513703+lechador@users.noreply.github.com>
2024-12-20 03:19:42 +0400

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog_newsroom',
         'USER': 'root',
-        'PASSWORD': 'Gran1408!',
+        'PASSWORD': '',
         'HOST':'localhost',
         'PORT':'3306',
     }
@@ -132,7 +132,7 @@ EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '46ff79001@smtp-brevo.com'
-EMAIL_HOST_PASSWORD = 'xsmtpsib-4bffbe0c1a810564f3ceb7e095b84d383ebb12653d6672f3fb8655ccaadf4057-a9hXzSWIM6ZtKCAJ'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = '46ff79001@smtp-brevo.com'
 
 FRONTEND_URL= 'https://newsroom-front.vercel.app'
